742c6add880fc8360db97936afd50cfe5687c5eb
ajpinkerton <ajpinkerton@users.noreply.github.com>
2021-10-05 21:14:52 -0400

    initial commit wip

---
+++ SqlDependencyHelper/SqlDependencyHelper.Database/DependentObject.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DependentObject]
+(
+	[DependentObjectID] INT NOT NULL,
+	[DatabaseName] VARCHAR(255),
+	[SchemaName] VARCHAR(255),
+	[ObjectName] VARCHAR(255),
+	[ObjectType] VARCHAR(255),
+	CONSTRAINT PK_DependentObject PRIMARY KEY (DependentObjectID)
+)
