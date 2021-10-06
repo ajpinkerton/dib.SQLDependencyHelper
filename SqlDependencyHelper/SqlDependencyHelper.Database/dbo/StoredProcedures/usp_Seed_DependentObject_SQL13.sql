71d44e44ff04cc524068cfe14b3c62529774a023
ajpinkerton <ajpinkerton@users.noreply.github.com>
2021-10-05 21:38:22 -0400

    wip

---
+++ SqlDependencyHelper/SqlDependencyHelper.Database/dbo/StoredProcedures/usp_Seed_DependentObject_SQL13.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[usp_Seed_DependentObject_SQL13]	
+AS
+BEGIN	
+	TRUNCATE TABLE [dbo].[DependentObject];
+
+	INSERT INTO [dbo].[DependentObject]([DatabaseName],[SchemaName],[ObjectName],[ObjectType])
+	VALUES ('MDF0880_Member','dbo','MemberUnit','Table');
+END
